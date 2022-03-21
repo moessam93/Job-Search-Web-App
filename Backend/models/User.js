7100bab17d4ccc56d0ee47adceffbd3afb8aa80e
Mohamed Essam <moessam@Mohameds-Air.home>
2022-03-21 16:49:00 +0200

    create User database model

---
+++ Backend/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'user must have an email']
+  },
+  firstName: {
+    type: String,
+    required: [true, 'user must have a first name']
+  },
+  lastName: {
+    type: String,
+    required:[true,'user must have a last name']
+  },
+  hash: {
+    type: String,
+    required: true
+  },
+  salt: {
+    type: String,
+    required:true
+},
+createdAt: {
+  type: Date,
+  default: Date.now(),
+},
+});
+
+module.exports = mongoose.model('User', userSchema);
