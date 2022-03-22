94e6b336cff3c1549c73444e3d87952b3931737c
Mohamed Essam <moessam@Mohameds-Air.home>
2022-03-22 21:19:35 +0200

    add authentication middleware

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const {createUser} = require('../controllers/users');
+const {createUser,viewProfile,updateProfile} = require('../controllers/users');
 const passport = require('passport');
+const {isAuth} = require('../config/passport');
 
 router.route('/register').post(createUser);
+
+router.route('/profile').get(isAuth,viewProfile);
+
+router.route('/profile/update/').patch(isAuth,updateProfile);
+
 router.route('/login').post(passport.authenticate('local',{failureRedirect:'/login-failure',successRedirect:'/login-success'}));
+
 router.route('/logout').get((req,res,next)=>{
     req.logout();
+    res.send("User Logged Out");
 });
 
 router.route('/login-success').get((req,res,next)=>{
