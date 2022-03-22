60326e04063a6a123e7879e2d7dce044f2101e7a
Mohamed Essam <moessam@Mohameds-Air.home>
2022-03-22 21:19:12 +0200

    update createUser, viewProfile, updateProfile controllers

--- Backend/controllers/users.js
+++ Backend/controllers/users.js
@@ -1,6 +1,7 @@
 const DB = require('../database/connect');
 const { genPassword } = require('../lib/passwordUtils');
 const User = require('../models/User');
+const UserProfile = require('../models/UserProfile');
 
 const createUser = (req, res, next) => {
     const saltHash = genPassword(req.body.password);
@@ -14,16 +15,38 @@ const createUser = (req, res, next) => {
         hash: hash,
         salt: salt
     })
-    console.log(newUser);
+
     User.findOne({ email: newUser.email }).then((user) => {
         if (user) {
             res.status(409).send("User Already Exists");
         }
         else {
             User.insertMany(newUser);
+            const newUserProfile = new UserProfile({
+                userID:newUser._id,
+                generalInfo:{
+                    firstName:newUser.firstName,
+                    lastName:newUser.lastName
+                },
+                contactInfo:{email:newUser.email}
+            })
+            UserProfile.insertMany(newUserProfile);
             res.status(201).send("User Created Successfully");
         }
     });
 }
 
-module.exports = { createUser };
+const viewProfile = (req,res,next)=>{
+    UserProfile.findOne({userID:req.user._id}).then((userProfile)=>{
+        res.status(200).json(userProfile);
+    });
+    
+}
+
+//TIP: Create one updateProfile Controller
+const updateProfile = async (req,res,next)=>{
+    const profile = await UserProfile.findOneAndUpdate({userID:req.user._id},req.body,{new:true});
+    res.status(201).send("Update Success");
+}
+
+module.exports = { createUser, viewProfile,updateProfile};
